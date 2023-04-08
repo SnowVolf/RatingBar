5d016838dbb8e6bf08b1f5c1c57ff949d088e8f8
SVolf <aftervolf@gmail.com>
2023-04-08 15:37:21 +0700

    Initial commit

---
+++ app/src/main/java/com/iarcuschin/simpletraingbarsample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iarcuschin.simpletraingbarsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iarcuschin.simpletraingbar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
